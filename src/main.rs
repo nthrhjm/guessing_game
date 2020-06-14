ea1f167ff666d3acb9232a56da1909c3a8d6195e
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-06-14 15:32:41 +0900

    update main.rs
    rand クレートへの依存を追加
    secret_numberを生成するコードを追加

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+extern crate rand;
+
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101); //1~100までの乱数を生成
+    println!("The secret number is: {}", secret_number);
     println!("Please input your guess.");
-    let mut guess = String::new();
+    let mut guess = String::new(); //空文字列を可変変数guessに代入
     io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        .read_line(&mut guess) //標準入力にguessを渡す（標準入力の内容をguessに入力)
+        .expect("Failed to read line"); //read_line()が返すResultがErrの場合クラッシュさせる
     println!("You guessed: {}", guess);
 }
