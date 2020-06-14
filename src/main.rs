e8c075603f845647f8a4862b37b9c84fd3df7d1a
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-06-14 16:20:38 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::io;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101); //1~100までの乱数を生成
-    println!("The secret number is: {}", secret_number);
+                                                              // println!("The secret number is: {}", secret_number);
     loop {
         println!("Please input your guess.");
         let mut guess = String::new(); //空文字列を可変変数guessに代入
